683f5b07a23232c3a2b07477b9d813b0c68edc89
Soongle <dydtns9815@naver.com>
2023-05-07 21:10:36 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+// const dummyList = [
+//     {
+//         id: 1,
+//         author: "soongle",
+//         content: "hi 1",
+//         emotion: 1,
+//         created_date: new Date().getTime(),
+//     },
+//     {
+//         id: 2,
+//         author: "gildong",
+//         content: "hi 2",
+//         emotion: 3,
+//         created_date: new Date().getTime(),
+//     },
+//     {
+//         id: 3,
+//         author: "amugae",
+//         content: "hi 3",
+//         emotion: 5,
+//         created_date: new Date().getTime(),
+//     },
+// ];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    const dataId = useRef(0);
+
+    const onCreate = (author, content, emotion) => {
+        const created_date = new Date().getTime();
+        const newItem = {
+            author,
+            content,
+            emotion,
+            created_date,
+            id: dataId.current,
+        };
+        dataId.current += 1;
+        setData([newItem, ...data]);
+    };
+
+    const onRemove = (targetId) => {
+        console.log(`${targetId}가 삭제되었습니다`);
+        setData(data.filter((it) => it.id !== targetId));
+    };
+
+    const onEdit = (targetId, newContent) => {
+        setData(
+            data.map((it) =>
+                it.id === targetId ? { ...it, content: newContent } : it
+            )
+        );
+    };
+
+    return (
+        <div className="App">
+            <DiaryEditor onCreate={onCreate} />
+            <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
+        </div>
+    );
 }
 
 export default App;
